19588ef69e6af69c6d1a04536a19e2b670811dee
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-02 17:12:48 +0530

    Bharti-ADD : Third test case for wrong file type

--- test_census_analysis.py
+++ test_census_analysis.py
@@ -24,3 +24,11 @@ def test_raises_exception_on_wrong_file():
     with pytest.raises(CensusAnalyserError):
         csv_loader = CSVLoader(CENSUS_CSV_WRONG_FILE_PATH)
         csv_loader.record_counter()
+
+# check if file type is right or not
+
+
+def test_raises_exception_on_wrong_file_type():
+    with pytest.raises(CensusAnalyserError):
+        csv_loader = CSVLoader(CENSUS_CSV_WRONG_FILE_TYPE)
+        csv_loader.record_counter()
