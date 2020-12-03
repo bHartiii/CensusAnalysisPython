258e54c2bb863ce8441e33a74fcf5a08bb60c4a1
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-03 21:40:48 +0530

    Bharti-REFACTOR2 : Refactored code to apply delegation principle

--- csv_generator.py
+++ csv_generator.py
@@ -1,8 +1,9 @@
-
-class CSVGenerator:
+class CSVGenerator(object):
 
     def __init__(self, header, path):
         self.path = path
         self.header = header
+
+
 
 
--- test_census_analysis.py
+++ test_census_analysis.py
@@ -1,7 +1,6 @@
 import pytest
 
 from com.bridgelabz.census.census_analyzer_error import CensusAnalyserError
-from com.bridgelabz.census.csv_generator import CSVGenerator
 from com.bridgelabz.census.CSVLoader import CSVFileLoader
 from com.bridgelabz.census.csv_generator import CSVGenerator
 from com.bridgelabz.census.Csv_header import IndiaCensusCSV
@@ -22,8 +21,7 @@ CENSUS_CSV_WRONG_STATE_CSV_DELIMITER = "/Users/LENOVO/PycharmProjects/census_ana
 
 def test_record_counter():
     csv = CSVGenerator(IndiaCensusCSV(), CENSUS_CSV_FILE_PATH)
-    csv_loader = CSVFileLoader()
-    csv_loader.generateCSV(csv.header, csv.path)
+    csv_loader = CSVFileLoader(csv)
     assert csv_loader.record_counter_in_csv() == 29
 
 # check if length of record in state csv file is same or not
@@ -31,8 +29,7 @@ def test_record_counter():
 
 def test_record_counter_in_state_csv():
     csv = CSVGenerator(StateCSVHeader(), CENSUS_STATE_CSV_FILE_PATH)
-    csv_loader = CSVFileLoader()
-    csv_loader.generateCSV(csv.header, csv.path)
+    csv_loader = CSVFileLoader(csv)
     assert csv_loader.record_counter_in_csv() == 37
 
 # check if file path is correct or not
@@ -41,8 +38,7 @@ def test_record_counter_in_state_csv():
 def test_raises_exception_on_wrong_file():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(IndiaCensusCSV(), CENSUS_CSV_WRONG_FILE_PATH)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if state csv file is right or not
@@ -51,8 +47,7 @@ def test_raises_exception_on_wrong_file():
 def test_raises_exception_on_wrong_state_file():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(StateCSVHeader(), CENSUS_CSV_WRONG_FILE_PATH)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if file type is right or not
@@ -61,8 +56,7 @@ def test_raises_exception_on_wrong_state_file():
 def test_raises_exception_on_wrong_file_type():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(IndiaCensusCSV(), CENSUS_CSV_WRONG_FILE_TYPE)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if state csv file type is right or not
@@ -71,8 +65,7 @@ def test_raises_exception_on_wrong_file_type():
 def test_raises_exception_on_wrong_state_file_type():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(StateCSVHeader(), CENSUS_CSV_WRONG_FILE_TYPE)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if delimiter is right or not
@@ -81,8 +74,7 @@ def test_raises_exception_on_wrong_state_file_type():
 def test_raises_exception_on_wrong_delimiter():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(IndiaCensusCSV(), CENSUS_CSV_WRONG_DELIMITER)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if delimiter in state csv is right or not
@@ -91,8 +83,7 @@ def test_raises_exception_on_wrong_delimiter():
 def test_raises_exception_on_wrong_delimiter_in_state_csv():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(StateCSVHeader(), CENSUS_CSV_WRONG_STATE_CSV_DELIMITER)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if file header is right or not
@@ -101,8 +92,7 @@ def test_raises_exception_on_wrong_delimiter_in_state_csv():
 def test_raises_exception_on_wrong_header():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(IndiaCensusCSV(), CENSUS_STATE_CSV_FILE_PATH)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
 
 # check if state csv file header is right or not
@@ -111,6 +101,5 @@ def test_raises_exception_on_wrong_header():
 def test_raises_exception_on_state_csv_wrong_header():
     with pytest.raises(CensusAnalyserError):
         csv = CSVGenerator(StateCSVHeader(), CENSUS_CSV_FILE_PATH)
-        csv_loader = CSVFileLoader()
-        csv_loader.generateCSV(csv.header, csv.path)
+        csv_loader = CSVFileLoader(csv)
         csv_loader.record_counter_in_csv()
