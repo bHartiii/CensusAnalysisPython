a3b15e91d8451d1d5039b1b362d5c2aabdfe3719
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-04 23:14:13 +0530

    Bharti-ADD : Sorting method is added to return data in json format

--- CSVReader.py
+++ CSVReader.py
@@ -1,3 +1,7 @@
+import json
+
+from pandas.io.json import to_json
+
 from com.bridgelabz.census.CSV_header import IndiaCensusCSV
 from com.bridgelabz.census.StateCSVHeader import StateCSVHeader
 from com.bridgelabz.census.CSVFileLoader import CSVFileLoader
@@ -15,5 +19,16 @@ class CSVFileReader:
         """
         return len(self.main.load_csv())
 
+    def sort_data_in_csv(self, sorting_key):
+        """
+        sorts csv data according to sorting key
+        :return:sorted data in json format
+        """
+        data_dict = {}
+        data = self.main.load_csv().sort_values(sorting_key)
+        for x in data.values:
+            data_list = list(x)
+            data_dict[data_list[0]] = data_list
+        return json.dumps(data_dict)
 
 
--- test_census_analysis.py
+++ test_census_analysis.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from com.bridgelabz.census.Census_analyzer_exception import CensusAnalyserError
@@ -5,99 +7,62 @@ from com.bridgelabz.census.CSVReader import CSVFileLoader, CSVFileReader
 from com.bridgelabz.census.CSV_header import IndiaCensusCSV
 from com.bridgelabz.census.StateCSVHeader import StateCSVHeader
 
-
 CENSUS_CSV_FILE_PATH = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCensusData.csv"
-CENSUS_STATE_CSV_FILE_PATH = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCode.csv"
-
-CENSUS_CSV_WRONG_FILE_PATH = "CSVFile/IndiaStateCensusData.csv"
-CENSUS_CSV_WRONG_FILE_TYPE = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCensusData.pdf"
+STATE_CODE_CSV_FILE_PATH = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCode.csv"
+CSV_WRONG_FILE_PATH = "CSVFile/IndiaStateCensusData.csv"
+CSV_WRONG_FILE_TYPE = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCensusData.pdf"
+CENSUS_CSV_WRONG_HEADER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCensusData_WrongHeader.csv"
+STATE_CODE_CSV_WRONG_HEADER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCode_WrongHeader.csv"
 CENSUS_CSV_WRONG_DELIMITER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCensusDataSpace.csv"
-CENSUS_CSV_WRONG_STATE_CSV_DELIMITER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCodeSemicolon.csv"
+STATE_CODE_CSV_WRONG_DELIMITER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census/data/IndiaStateCodeSemicolon.csv"
 
 
-# check if length of record is same or not
+@pytest.fixture
+def census_csv_file():
+    csv_file = CSVFileLoader(IndiaCensusCSV(), CENSUS_CSV_FILE_PATH)
+    csv = CSVFileReader(csv_file)
+    return csv
+
+# check if given path, file type, delimiter and header is correct or not
 
 
-def test_givenIndiaCensusCSVFile_WhenCounted_ShouldReturnRecordsCount():
-    csv = CSVFileLoader(IndiaCensusCSV(), CENSUS_CSV_FILE_PATH)
+@pytest.mark.parametrize("header,path,expected", [
+    (IndiaCensusCSV(), CSV_WRONG_FILE_TYPE, CensusAnalyserError),
+    (StateCSVHeader(), CSV_WRONG_FILE_TYPE, CensusAnalyserError),
+    (IndiaCensusCSV(), CSV_WRONG_FILE_PATH, CensusAnalyserError),
+    (StateCSVHeader(), CSV_WRONG_FILE_PATH, CensusAnalyserError),
+    (IndiaCensusCSV(), CENSUS_CSV_WRONG_DELIMITER, CensusAnalyserError),
+    (StateCSVHeader(), STATE_CODE_CSV_WRONG_DELIMITER, CensusAnalyserError),
+    (IndiaCensusCSV(), CENSUS_CSV_WRONG_HEADER, CensusAnalyserError),
+    (StateCSVHeader(), STATE_CODE_CSV_WRONG_HEADER, CensusAnalyserError),
+])
+def test_givenCSVFile_WhenLoaded_ShouldHaveCorrectPathTypeHeaderAndDelimiter(header, path, expected):
+    with pytest.raises(expected):
+        csv = CSVFileLoader(header, path)
+        csv.load_csv()
+
+# check if length of record is same or not in given csv file
+
+
+@pytest.mark.parametrize("header,path, expected", [
+    (IndiaCensusCSV(), CENSUS_CSV_FILE_PATH, 29),
+    (StateCSVHeader(), STATE_CODE_CSV_FILE_PATH, 37),
+])
+def test_givenCSVFile_WhenCounted_ShouldReturnRecordsCount(header, path, expected):
+    csv = CSVFileLoader(header, path)
     csv_file = CSVFileReader(csv)
-    assert csv_file.record_counter_in_csv() == 29
+    assert csv_file.record_counter_in_csv() == expected
 
-# check if length of record in state csv file is same or not
-
-
-def test_givenStateCodeCSVFile_WhenCounted_ShouldReturnRecordsCount():
-    csv = CSVFileLoader(StateCSVHeader(), CENSUS_STATE_CSV_FILE_PATH)
-    csv_file = CSVFileReader(csv)
-    assert csv_file.record_counter_in_csv() == 37
-
-# check if file path is correct or not
+# check if IndiaCensusData.csv file is sorted or not
 
 
-def test_givenIndiaCensusCSVFile_WhenLoaded_IfPathIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(IndiaCensusCSV(), CENSUS_CSV_WRONG_FILE_PATH)
-        csv.load_csv()
+def test_givenCSVFile_WhenSorted_IfNot_ShouldRaiseCustomException(census_csv_file):
+    data = json.loads(census_csv_file.sort_data_in_csv("State"))
+    if list(data.keys())[0] != "Andhra Pradesh":
+        raise CensusAnalyserError("File is not sorted!!!")
+    if list(data.keys())[len(data)-1] != "West Bengal":
+        raise CensusAnalyserError("File is not sorted!!!")
 
 
-# check if state csv file is right or not
 
 
-def test_givenStateCodeCSVFile_WhenLoaded_IfPathIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(StateCSVHeader(), CENSUS_CSV_WRONG_FILE_PATH)
-        csv.load_csv()
-
-
-# check if file type is right or not
-
-
-def test_givenIndiaCensusCSVFile_WhenLoaded_IfExtensionIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(IndiaCensusCSV(), CENSUS_CSV_WRONG_FILE_TYPE)
-        csv.load_csv()
-
-
-# check if state csv file type is right or not
-
-
-def test_givenStateCodeCSVFile_WhenLoaded_IfExtensionIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(StateCSVHeader(), CENSUS_CSV_WRONG_FILE_TYPE)
-        csv.load_csv()
-
-
-# check if delimiter is right or not
-
-
-def test_givenIndiaCensusCSVFile_WhenLoaded_IfDelimiterIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(IndiaCensusCSV(), CENSUS_CSV_WRONG_DELIMITER)
-        csv.load_csv()
-
-
-# check if delimiter in state csv is right or not
-
-
-def test_givenStateCodeCSVFile_WhenLoaded_IfDelimiterIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(StateCSVHeader(), CENSUS_CSV_WRONG_STATE_CSV_DELIMITER)
-        csv.load_csv()
-
-
-# check if file header is right or not
-
-
-def test_givenIndiaCensusCSVFile_WhenLoaded_IfHeaderIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(IndiaCensusCSV(), CENSUS_STATE_CSV_FILE_PATH)
-        csv.load_csv()
-
-
-# check if state csv file header is right or not
-
-
-def test_givenStateCodeCSVFile_WhenLoaded_IfHeaderIsWrong_ShouldRaiseCustomException():
-    with pytest.raises(CensusAnalyserError):
-        csv = CSVFileLoader(StateCSVHeader(), CENSUS_CSV_FILE_PATH)
-        csv.load_csv()
