2f807a10643647fc0157bd7326a03eeaff5dcf49
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-06 01:24:32 +0530

    Bharti-REFACTORED : Test cases for sorting are refactored by parameterized testcases

--- census_analyzer_testing/test_census_analysis.py
+++ census_analyzer_testing/test_census_analysis.py
@@ -15,6 +15,7 @@ CENSUS_CSV_WRONG_HEADER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bri
 STATE_CODE_CSV_WRONG_HEADER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census_analyzer/data/IndiaStateCode_WrongHeader.csv"
 CENSUS_CSV_WRONG_DELIMITER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census_analyzer/data/IndiaStateCensusDataSpace.csv"
 STATE_CODE_CSV_WRONG_DELIMITER = "/Users/LENOVO/PycharmProjects/census_analysis/com/bridgelabz/census_analyzer/data/IndiaStateCodeSemicolon.csv"
+CENSUS_ANALYZER_CUSTOM_EXCEPTION = CensusAnalyserError("File is not sorted")
 
 
 @pytest.fixture
@@ -30,9 +31,8 @@ def state_csv_file():
     state_csv_file = CSVFileReader(csv_file)
     return state_csv_file
 
+
 # check if given path, file type, delimiter and header is correct or not
-
-
 @pytest.mark.parametrize("header,path,expected", [
     (IndiaCensusCSV(), CSV_WRONG_FILE_TYPE, CensusAnalyserError),
     (StateCSVHeader(), CSV_WRONG_FILE_TYPE, CensusAnalyserError),
@@ -48,9 +48,8 @@ def test_givenCSVFile_WhenLoaded_ShouldHaveCorrectPathTypeHeaderAndDelimiter(hea
         csv = CSVFileLoader(header, path)
         csv.load_csv()
 
+
 # check if length of record is same or not in given csv file
-
-
 @pytest.mark.parametrize("header,path, expected", [
     (IndiaCensusCSV(), CENSUS_CSV_FILE_PATH, 29),
     (StateCSVHeader(), STATE_CODE_CSV_FILE_PATH, 37),
@@ -60,45 +59,43 @@ def test_givenCSVFile_WhenCounted_ShouldReturnRecordsCount(header, path, expecte
     csv_file = CSVFileReader(csv)
     assert csv_file.record_counter_in_csv() == expected
 
-# check if IndiaCensusData.csv file is sorted or not
+
+# check if IndiaCensusData.csv file is sorted and first row is matched or not
+@pytest.mark.parametrize("sorting_key, list_element, expected", [
+    ("State", "Andhra Pradesh", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+    ("Population", "Uttar Pradesh", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+    ("DensityPerSqKm", "Bihar", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+    ("AreaInSqKm", "Rajasthan", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+])
+def test_givenIndiaCensusCSVFile_WhenSorted_IfFirstRowIsNotMatched_ShouldRaiseCustomException(census_csv_file,
+                                                                                              sorting_key, list_element, expected):
+    data = json.loads(census_csv_file.sort_data_in_csv(sorting_key))
+    if list(data.keys())[0] != list_element:
+        raise expected
 
 
-def test_givenIndiaCensusCSVFile_WhenSorted_IfNot_ShouldRaiseCustomException(census_csv_file):
-    data = json.loads(census_csv_file.sort_data_in_csv("State"))
-    if list(data.keys())[0] != "Andhra Pradesh" and list(data.keys())[len(data)-1] != "West Bengal":
-        raise CensusAnalyserError("File is not sorted!!!")
+# check if IndiaCensusData.csv file is sorted and last row is matched or not
+@pytest.mark.parametrize("sorting_key, list_element, expected", [
+    ("State", "West Bengal", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+    ("Population", "Sikkim", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+    ("DensityPerSqKm", "Arunachal", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+    ("AreaInSqKm", "Goa", CENSUS_ANALYZER_CUSTOM_EXCEPTION),
+])
+def test_givenIndiaCensusCSVFile_WhenSorted_IfLastRowIsNotMatched_ShouldRaiseCustomException(census_csv_file, sorting_key,list_element, expected):
+    data = json.loads(census_csv_file.sort_data_in_csv(sorting_key))
+    if list(data.keys())[-1] != list_element:
+        raise expected
+
 
 # check if IndiaStateCode.csv file is sorted or not
-
-
-def test_givenStateCodeCSV_WhenSorted_IfNot_ShouldRaiseCustomException(state_csv_file):
+def test_givenStateCodeCSV_WhenSorted_IfFirstRowNotMatched_ShouldRaiseCustomException(state_csv_file):
     data = json.loads(state_csv_file.sort_data_in_csv("StateCode"))
-    if list(data.get(list(data.keys())[0]))[3] != "AD" and list(data.get(list(data.keys())[len(data) - 1]))[3] != "WB":
-        raise CensusAnalyserError("File is not sorted")
+    if list(data.get(list(data.keys())[0]))[3] != "AD":
+        raise CENSUS_ANALYZER_CUSTOM_EXCEPTION
 
 
-# check if IndiaStateCode.csv file is sorted or not according to population from most to least
-
-
-def test_givenStateCodeCSV_WhenSortedAccordingToPopulation_IfNot_ShouldRaiseCustomException(census_csv_file):
-    data = json.loads(census_csv_file.sort_data_in_csv("Population"))
-    if list(data.keys())[0] != "Uttar Pradesh" and list(data.keys())[len(data) - 1] != "Sikkim":
-        raise CensusAnalyserError("File is not sorted")
-
-
-# check if IndiaStateCode.csv file is sorted or not according to population density from most to least
-
-
-def test_givenStateCodeCSV_WhenSortedAccordingToDensity_IfNot_ShouldRaiseCustomException(census_csv_file):
-    data = json.loads(census_csv_file.sort_data_in_csv("DensityPerSqKm"))
-    if list(data.keys())[0] != "Bihar" and list(data.keys())[len(data) - 1] != "Arunachal Pradesh":
-        raise CensusAnalyserError("File is not sorted")
-
-
-# check if IndiaStateCode.csv file is sorted or not according to largest area to smallest
-
-
-def test_givenStateCodeCSV_WhenSortedAccordingToArea_IfNot_ShouldRaiseCustomException(census_csv_file):
-    data = json.loads(census_csv_file.sort_data_in_csv("AreaInSqKm"))
-    if list(data.keys())[0] != "Rajasthan" and list(data.keys())[len(data) - 1] != "Goa":
-        raise CensusAnalyserError("File is not sorted")
+# check if IndiaStateCode.csv file is sorted or not
+def test_givenStateCodeCSV_WhenSorted_IfLastRowNotMatched_ShouldRaiseCustomException(state_csv_file):
+    data = json.loads(state_csv_file.sort_data_in_csv("StateCode"))
+    if list(data.get(list(data.keys())[-1]))[3] != "WB":
+        raise CENSUS_ANALYZER_CUSTOM_EXCEPTION
